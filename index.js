55774b95f6c04d01fb947a0a15f579d913452b0a
unitygamecodes <117928623+unitygamecodes@users.noreply.github.com>
2022-11-11 03:02:56 +0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 const host = '0.0.0.0';
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, host);
 
