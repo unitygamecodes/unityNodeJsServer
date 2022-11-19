d3bce8300e574ae76c46e336a8c0d52343de1ea7
unitygamecodes <117928623+unitygamecodes@users.noreply.github.com>
2022-11-19 05:37:59 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dgram = require('dgram');
 const server = dgram.createSocket('udp4');
 
 const hostnam = '0.0.0.0';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 server.on('error', (err) => {
 
